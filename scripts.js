1d5403b124856b3f124aa36cdbdc3da89ca20010
Admin-IGET <smmweplayer2@gmail.com>
2025-02-26 18:00:57 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -266,7 +266,7 @@ function openApp(appName) {
         game: "https://admin-iget.github.io/test/Uvikhry.html",
         store: "https://admin-iget.github.io/test/UvikObchod.html",
         video: "https://admin-iget.github.io/test/yt.html" // New Video App
-        "UvikMail-BETA": "https://admin-iget.github.io/test/UvikMail"
+        UvikMail: "https://admin-iget.github.io/test/UvikMail"
     };
 
     if (urls[appName]) {
